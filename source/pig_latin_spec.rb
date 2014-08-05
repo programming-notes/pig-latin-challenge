324923759896276fd29acbbd245c9cce9fbc1539
Matt Baker <mbaker.pdx@gmail.com>
2014-08-05 13:22:36 -0500

    Remove erroneous file and correct test description

--- source/pig_latin_spec.rb
+++ source/pig_latin_spec.rb
@@ -3,5 +3,5 @@ require_relative('pig_latin')
 describe 'Converting to Pig Latin' do
 end
 
-describe 'Converting from Pig Latin to English' do
+describe 'Converting an English sentence to Pig Latin' do
 end
