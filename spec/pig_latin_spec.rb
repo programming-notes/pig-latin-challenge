ac07c03fc724a7bbca82b39a8df2478dd9814a10
Leilair & Nick Himmel <pair+leilagleich.nickhimmel@devbootcamp.com>
2016-12-06 11:03:56 -0500

    add two rspec test for release O'

--- spec/pig_latin_spec.rb
+++ spec/pig_latin_spec.rb
@@ -1,7 +1,14 @@
 require_relative('../pig_latin')
 
 describe 'Converting single words to Pig Latin' do
+  it "vowel" do
+    expect convert_word_to_pig_latin('apple').to eq 'apple'
+  end
+
+  it "consonant" do
+    expect convert_word_to_pig_latin('ruby').to eq 'ubyray'
+  end
+
 end
 
-describe 'Converting a sentence to Pig Latin' do
-end
+
