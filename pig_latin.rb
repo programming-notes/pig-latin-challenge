ecccd154f6da9664e66949ccf64e3d60803e3643
Leilair & Nick Himmel <pair+leilagleich.nickhimmel@devbootcamp.com>
2016-12-06 11:29:25 -0500

    Complete first draft of pig latin method

--- pig_latin.rb
+++ pig_latin.rb
@@ -1,1 +1,16 @@
-# Your code here
+def convert_word_to_pig_latin(not_pig_latin)
+  if not_pig_latin.match(/[aeiou]/)
+    not_pig_latin
+  else
+    not_pig_latin + not_pig_latin.slice!(0) + "ay"
+  end
+end
+
+
+
+p convert_word_to_pig_latin("strawberry")
+
+
+
+
+
--- spec/pig_latin_spec.rb
+++ spec/pig_latin_spec.rb
@@ -1,12 +1,13 @@
 require_relative('../pig_latin')
 
 describe 'Converting single words to Pig Latin' do
+
   it "vowel" do
-    expect convert_word_to_pig_latin('apple').to eq 'apple'
+    expect(convert_word_to_pig_latin('apple')).to eq 'apple'
   end
 
   it "consonant" do
-    expect convert_word_to_pig_latin('ruby').to eq 'ubyray'
+    expect(convert_word_to_pig_latin('ruby')).to eq 'ubyray'
   end
 
 end
